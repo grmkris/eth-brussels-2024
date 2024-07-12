c8ade87058bf39b63a82b5b14b44c05fb759b5df
grmkris <kristjan.grm1@gmail.com>
2024-07-12 23:26:05 +0200

    aaa

--- apps/frontend/src/components/Profile.tsx
+++ apps/frontend/src/components/Profile.tsx
@@ -1,9 +1,21 @@
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { useSignMessage } from "wagmi";
 
 export function ConnectWallet() {
   return <Profile />;
 }
 
 const Profile = () => {
-  return <DynamicWidget />;
+  const signMessage = useSignMessage();
+
+  return (
+    <>
+      <button
+        onClick={() => signMessage.signMessage({ message: "Hello, world!" })}
+      >
+        Sign message
+      </button>
+      <DynamicWidget />;
+    </>
+  );
 };
