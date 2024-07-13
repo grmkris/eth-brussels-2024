54cfbebb316645cd416b1cf1cb95104a0cff1a29
TilenOman50 <tilko.kobilko@gmail.com>
2024-07-13 14:52:18 +0200

    openapi change

--- apps/frontend/src/openapi.ts
+++ apps/frontend/src/openapi.ts
@@ -1,110 +1,340 @@
 export default {
-  openapi: "3.0.0",
-  info: {
-    version: "1.0.0",
-    title: "API",
+  "openapi": "3.0.0",
+  "info": {
+    "version": "1.0.0",
+    "title": "API"
   },
-  components: {
-    schemas: {},
-    parameters: {},
+  "components": {
+    "schemas": {},
+    "parameters": {}
   },
-  paths: {
+  "paths": {
     "/players/{id}": {
-      get: {
-        operationId: "retrievePlayer",
-        summary: "Retrieve player",
-        tags: ["Players"],
-        description: "Retrieve a player by ID",
-        parameters: [
-          {
-            schema: {
-              type: "string",
-            },
-            required: true,
-            name: "id",
-            in: "path",
-          },
+      "get": {
+        "operationId": "retrievePlayer",
+        "summary": "Retrieve player",
+        "tags": [
+          "Players"
         ],
-        responses: {
-          "200": {
-            description: "Retrieved player successfully.",
-            content: {
-              "application/json": {
-                schema: {
-                  type: "object",
-                  properties: {
-                    id: {
-                      type: "string",
-                      format: "uuid",
-                    },
-                    address: {
-                      type: "string",
-                    },
-                    lastMove: {
-                      type: "string",
-                    },
-                    createdAt: {
-                      type: "string",
-                    },
-                  },
-                  required: ["id", "address", "lastMove", "createdAt"],
-                },
-              },
+        "description": "Retrieve a player by ID",
+        "parameters": [
+          {
+            "schema": {
+              "type": "string"
             },
-          },
-        },
-      },
+            "required": true,
+            "name": "id",
+            "in": "path"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Retrieved player successfully.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "string",
+                      "format": "uuid"
+                    },
+                    "address": {
+                      "type": "string"
+                    },
+                    "lastMove": {
+                      "type": "string"
+                    },
+                    "createdAt": {
+                      "type": "string"
+                    }
+                  },
+                  "required": [
+                    "id",
+                    "address",
+                    "lastMove",
+                    "createdAt"
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
     },
     "/players": {
-      post: {
-        operationId: "createPlayer",
-        summary: "Create player",
-        tags: ["Players"],
-        description: "Create a player",
-        requestBody: {
-          content: {
+      "post": {
+        "operationId": "createPlayer",
+        "summary": "Create player",
+        "tags": [
+          "Players"
+        ],
+        "description": "Create a player",
+        "requestBody": {
+          "content": {
             "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  address: {
-                    type: "string",
-                  },
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "address": {
+                    "type": "string"
+                  }
                 },
-                required: ["address"],
-              },
-            },
-          },
+                "required": [
+                  "address"
+                ]
+              }
+            }
+          }
         },
-        responses: {
+        "responses": {
           "201": {
-            description: "Created player successfully.",
-            content: {
+            "description": "Created player successfully.",
+            "content": {
               "application/json": {
-                schema: {
-                  type: "object",
-                  properties: {
-                    id: {
-                      type: "string",
-                      format: "uuid",
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "string",
+                      "format": "uuid"
                     },
-                    address: {
-                      type: "string",
+                    "address": {
+                      "type": "string"
                     },
-                    lastMove: {
-                      type: "string",
+                    "lastMove": {
+                      "type": "string"
                     },
-                    createdAt: {
-                      type: "string",
-                    },
+                    "createdAt": {
+                      "type": "string"
+                    }
                   },
-                  required: ["id", "address", "lastMove", "createdAt"],
-                },
-              },
-            },
-          },
-        },
-      },
+                  "required": [
+                    "id",
+                    "address",
+                    "lastMove",
+                    "createdAt"
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
     },
-  },
+    "/games/{id}": {
+      "get": {
+        "operationId": "retrieveGame",
+        "summary": "Retrieve game",
+        "tags": [
+          "Games"
+        ],
+        "description": "Retrieve a game by ID",
+        "parameters": [
+          {
+            "schema": {
+              "type": "string"
+            },
+            "required": true,
+            "name": "id",
+            "in": "path"
+          }
+        ],
+        "requestBody": {
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "xCoordinate": {
+                    "type": "number"
+                  },
+                  "yCoordinate": {
+                    "type": "number"
+                  },
+                  "size": {
+                    "type": "number"
+                  }
+                },
+                "required": [
+                  "xCoordinate",
+                  "yCoordinate",
+                  "size"
+                ]
+              }
+            }
+          }
+        },
+        "responses": {
+          "200": {
+            "description": "Retrieved game successfully.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "string",
+                      "format": "uuid"
+                    },
+                    "status": {
+                      "type": "string",
+                      "enum": [
+                        "ongoing",
+                        "finished"
+                      ]
+                    },
+                    "winnerId": {
+                      "type": "string",
+                      "nullable": true,
+                      "format": "uuid"
+                    },
+                    "createdAt": {
+                      "type": "string"
+                    },
+                    "updatedAt": {
+                      "type": "string"
+                    },
+                    "map": {
+                      "type": "array",
+                      "items": {
+                        "type": "array",
+                        "items": {
+                          "type": "object",
+                          "properties": {
+                            "playerAddress": {
+                              "type": "string",
+                              "nullable": true
+                            }
+                          },
+                          "required": [
+                            "playerAddress"
+                          ]
+                        }
+                      }
+                    }
+                  },
+                  "required": [
+                    "id",
+                    "status",
+                    "winnerId",
+                    "createdAt",
+                    "updatedAt",
+                    "map"
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "/games": {
+      "get": {
+        "operationId": "listGames",
+        "summary": "List games",
+        "tags": [
+          "Games"
+        ],
+        "description": "List all games",
+        "responses": {
+          "200": {
+            "description": "Listed games successfully.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "array",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "id": {
+                        "type": "string",
+                        "format": "uuid"
+                      },
+                      "status": {
+                        "type": "string",
+                        "enum": [
+                          "ongoing",
+                          "finished"
+                        ]
+                      },
+                      "winnerId": {
+                        "type": "string",
+                        "nullable": true,
+                        "format": "uuid"
+                      },
+                      "createdAt": {
+                        "type": "string"
+                      },
+                      "updatedAt": {
+                        "type": "string"
+                      }
+                    },
+                    "required": [
+                      "id",
+                      "status",
+                      "winnerId",
+                      "createdAt",
+                      "updatedAt"
+                    ]
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      "post": {
+        "operationId": "createGame",
+        "summary": "Create game",
+        "tags": [
+          "Games"
+        ],
+        "description": "Create a game",
+        "responses": {
+          "201": {
+            "description": "Created game successfully.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "string",
+                      "format": "uuid"
+                    },
+                    "status": {
+                      "type": "string",
+                      "enum": [
+                        "ongoing",
+                        "finished"
+                      ]
+                    },
+                    "winnerId": {
+                      "type": "string",
+                      "nullable": true,
+                      "format": "uuid"
+                    },
+                    "createdAt": {
+                      "type": "string"
+                    },
+                    "updatedAt": {
+                      "type": "string"
+                    }
+                  },
+                  "required": [
+                    "id",
+                    "status",
+                    "winnerId",
+                    "createdAt",
+                    "updatedAt"
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 } as const;
